96b89e1b8bea2e1acd87314af02e3a67e30e7db9
Sabli Jr <sablijr@yahoo.com>
2023-05-16 02:54:19 +0200

    Make a portfolio

--- frontend/src/Pages/PortfolioPages/Portfolio.tsx
+++ frontend/src/Pages/PortfolioPages/Portfolio.tsx
@@ -1,28 +1,60 @@
+import { useEffect, useRef, useState } from "react";
 import "./Portfolio.css";
 
 import { FaEye } from "react-icons/fa";
+import AddAssets from "../../Components/PortfolioAssets/AddAssets";
 
-interface IProps {}
+const Portfolio: React.FC = () => {
+  const [AddingAssets, setAddingAssets] = useState(false);
+  const btnRef = useRef<HTMLButtonElement>(null);
+  const componentRef = useRef<HTMLDivElement>(null);
 
-const Portfolio: React.FC<IProps> = (props) => {
+  useEffect(() => {
+    let closeModel = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (
+        !btnRef?.current?.contains(e.target as HTMLButtonElement) &&
+        !componentRef?.current?.contains(e.target as Node)
+      ) {
+        setAddingAssets(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeModel as () => void);
+    return () =>
+      document.removeEventListener("mousedown", closeModel as () => void);
+  }, [AddingAssets]);
+
   return (
     <>
-      <div className='portfolio'>
-        <h2>Create your portfolio and see how your assets perform</h2>
+      <main className='portfolio'>
+        <h2>Create your portfolio and see how your assets perform.</h2>
 
         <div className='balance'>
-          <div>
+          <div className='balanceText'>
             <p>
               Current Balance <FaEye />
             </p>
-            <h3>10,000</h3>
+            <h3>$10,000.035</h3>
             <span>
-              + $10,000 <span>24h</span>
+              + $1,000 <span>24h</span>
             </span>
           </div>
-          <button>+ Add assets</button>
+          <div>
+            <button
+              ref={btnRef}
+              className='addAssetsBtn'
+              onClick={() => setAddingAssets(!AddingAssets)}>
+              + Add assets
+            </button>
+          </div>
         </div>
-      </div>
+
+        {AddingAssets && (
+          <div ref={componentRef}>
+            <AddAssets setAddingAssets={setAddingAssets} />
+          </div>
+        )}
+      </main>
     </>
   );
 };
