71f8d2685e923bdf08733cc8aaf26bba575d312d
Sabli Jr <sablijr@yahoo.com>
2023-05-26 13:18:15 +0200

    Optimizing for deployment

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,10 +5,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import axios from "axios";
 
-
 dotenv.config()
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://falconce.netlify.app",
+  })
+);
 
 //Getting the coins data
 app.get("/coins", (req: Request, resp: Response) => {
